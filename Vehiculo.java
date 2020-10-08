3ad670ffb504777b1efa09b6415c3c6c13c32b2c
emmanuel <emmvazquez@gmail.com>
2020-10-08 13:14:47 -0500

    Se crearon los métodos de acelerar

--- Vehiculo.java
+++ Vehiculo.java
@@ -5,6 +5,7 @@ public class Vehiculo {
 	private int Velocidad;
 	private String Marca;
 	private int Anio;
+	private int Revoluciones;
 
 	public Vehiculo() {
 		// TODO Auto-generated constructor stub
@@ -16,14 +17,25 @@ public class Vehiculo {
 		obj1.Velocidad = 0;
 		obj1.Marca = "Ford";
 		obj1.Anio = 2020;
+		obj1.Revoluciones=0;
 		
 		Vehiculo obj2 = new Vehiculo();
 		obj2.Color = "Azul";
 		obj2.Velocidad = 0;
 		obj2.Marca = "Chevrolet";
 		obj2.Anio = 2010;
+		obj2.Revoluciones=0;
 		
 		
+		obj1.encender();
+		obj1.acelerar();
+		obj1.acelerar();
+		
+		
+		obj2.encender();
+		
+		obj1.estado();
+		obj2.estado();
 		
 		//GET obtener los valores
 		//SET insertar nuevos valores 
@@ -34,9 +46,27 @@ public class Vehiculo {
 	// 2. opcional static
 	// 3. tipo de método [void = vacio, int, String , float, Double ]
 	public void encender() {
+		
+		this.setRevoluciones(1000);
 		System.out.println(this.getMarca() + " Está encendido" );
+		
+	
+		/*System.out.println("Hola");
+		System.out.println("8392983928");
+		System.out.println("Hola" + 9209320);*/
 	}
 
+	public void acelerar() {
+		this.setRevoluciones(this.getRevoluciones()+500);
+	}
+	
+	public void estado() {
+		System.out.println("R/s: " + this.getRevoluciones() );
+		
+	}
+	
+	
+	
 	public String getColor() {
 		return Color;
 	}
@@ -68,6 +98,15 @@ public class Vehiculo {
 	public void setAnio(int anio) {
 		Anio = anio;
 	}
+
+	public int getRevoluciones() {
+		return Revoluciones;
+	}
+
+	public void setRevoluciones(int revoluciones) {
+		Revoluciones = revoluciones;
+	}
+	
 	
 	
 
